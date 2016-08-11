0f4d557d9327b1f450fb06146be22e1eecec6055
Bhawani Parajuli and Stephanie Roth <bhawani@>
2016-08-11 08:22:18 -0700

    add data model for therestaurant.

--- app/app.components.ts
+++ app/app.components.ts
@@ -1,10 +1,15 @@
 import { Component } from 'angular2/core';
+import { Restaurant } from './restaurant.model';
 @Component({
   selector: 'my-app',
+  directives: [],
   template: `
-  <h1>Skeleton Angular2 App!</h1>
+  <h1>Restaurant Reviews</h1>
   `
 })
 export class AppComponent{
+  public restaurants: Restaurant[];
+  constructor(){
 
+  }
 }
---
+++ app/restaurant.model.ts
@@ -0,0 +1,5 @@
+export class Restaurant {
+  constructor(public name: string, public specialty: string, public address: string, public cost: string, public rating: number[], public waitTime: number[]){
+    
+  }
+}
